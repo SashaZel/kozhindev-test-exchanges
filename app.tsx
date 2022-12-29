2affede24115421522a907ce41dce16419d8e734
Alexander Zelenkov <lll555@yandex.ru>
2022-12-29 16:53:58 +0700

    init commit

---
+++ app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Ololo } from "./src/ololo";
+
+function App() {
+  return <div>
+    {"Great react setup from scratch without cra testing ts"}
+    <Ololo />
+  </div>;
+}
+export default App;
---
+++ src/ololo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface IOlolo {
+  'hiDev': string
+}
+
+export const Ololo = () => {
+
+  const myConst: IOlolo ={
+    "hiDev": "hello"
+  }
+
+  return (
+    <div>Ololo page</div>
+  );
+}
